533b8fb48c9262cc7ea2d4ee28982bd618127e6c
mhaecker@gmail.com <mhaecker@gmail.com@145117a4-22cd-11de-b44d-8590aa00aed0>
2010-03-22 20:49:59 +0000

    experimental changes to get rhino to work (still a total failure though)

--- spec/rhino.js
+++ spec/rhino.js
@@ -1,21 +1,20 @@
 
 // Latest env.js doesn't work with jspec (yet)
-load('./spec/support/env.rhino.js')
+load('spec/support/env.js')
 Envjs('spec/support/dummy.html')
-load('./spec/support/jquery.js')
-
-load('./spec/lib/jspec.js')
-load('./spec/lib/jspec.xhr.js')
 
 
-load('./spec/lib/jspec.jquery.js')
-
-load('./spec/lib/jspec.growl.js')
+load('spec/lib/jspec.js')
+load('spec/lib/jspec.xhr.js')
+load('spec/support/jquery.js')
+load('spec/lib/jspec.jquery.js')
+load('spec/lib/jspec.growl.js')
 
 load('lib/jquery.editinplace.js')
+
 load('spec/unit/spec.helper.js')
 
 JSpec
-.exec('spec/unit/spec.js')
-.run({ reporter: JSpec.reporters.Terminal, fixturePath: 'spec/fixtures' })
-.report()
+	.exec('spec/unit/spec.js')
+	.run({ reporter: JSpec.reporters.Terminal, fixturePath: 'spec/fixtures' })
+	.report()
