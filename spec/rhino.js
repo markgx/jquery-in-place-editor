c968c869d6edd94dc4ac0bf2f0bd3d89d792d6da
mhaecker@gmail.com <mhaecker@gmail.com@145117a4-22cd-11de-b44d-8590aa00aed0>
2010-03-22 20:46:09 +0000

    updated testrunners to allow mocking of ajax requests

--- spec/rhino.js
+++ spec/rhino.js
@@ -5,7 +5,7 @@ Envjs('spec/support/dummy.html')
 load('./spec/support/jquery.js')
 
 load('./spec/lib/jspec.js')
-// load('./spec/lib/jspec.xhr.js')
+load('./spec/lib/jspec.xhr.js')
 
 
 load('./spec/lib/jspec.jquery.js')
