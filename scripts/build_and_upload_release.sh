7bc187bdf384fedaf4d0b901f2035c89855a3d2a
mhaecker <mhaecker@145117a4-22cd-11de-b44d-8590aa00aed0>
2010-03-23 21:19:08 +0000

    added changelog updating to the release script

--- scripts/build_and_upload_release.sh
+++ scripts/build_and_upload_release.sh
@@ -1,19 +1,28 @@
 #!/bin/sh
 
+# TODO: consider to create / update the tag when running the release script
+# TODO: push changes to google code
+# TODO: find out how to save multiple push locations
+# claim commits for my other email address
+
 USAGE="Usage: $0 <bzr-tag-to-release> <googlecode-username> <googlecode-password>"
 
-RELEASE_TAG=${1:?$USAGE}
-USERNAME=${2:?$USAGE}
-PASSWORD=${3:?$USAGE}
+RELEASE_TAG=${1:? $USAGE}
+USERNAME=${2:? $USAGE}
+PASSWORD=${3:? $USAGE}
 
 cd "$(dirname "$0")/.."
 
+# update changelog
+bzr log --log-format=gnu-changelog > CHANGELOG
+bzr commit CHANGELOG -m "updated changelog before release"
+
+# build release zip
 mkdir -p build
+EXPORTED_FILE="build/jquery-editInPlace-v${RELEASE_TAG}.zip"
+bzr export --revision="$RELEASE_TAG" "$EXPORTED_FILE"
 
-FILENAME="build/jquery-editInPlace-v${RELEASE_TAG}.zip"
-
-bzr export --revision="$RELEASE_TAG"  "$FILENAME"
-
+# upload release zip
 SUMMARY="jQuery In Place Editor v${RELEASE_TAG}"
 python scripts/googlecode_upload.py \
 	--summary="$SUMMARY" \
@@ -21,6 +30,10 @@ python scripts/googlecode_upload.py \
 	--user="$USERNAME" \
 	--password="$PASSWORD" \
 	--labels="Type-Archive,OpSys-All" \
-	"$FILENAME"
+	"$EXPORTED_FILE"
 
+echo
+echo
 echo "Now you can feature this download at http://code.google.com/p/jquery-in-place-editor/downloads/list"
+echo
+echo
