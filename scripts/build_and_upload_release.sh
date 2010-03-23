f163f901d35bb9dad8c0c2e25cf204fd6cbef26e
mhaecker <mhaecker@145117a4-22cd-11de-b44d-8590aa00aed0>
2010-03-23 21:30:58 +0000

    updated release script to say what it does and tagg and push the release

--- scripts/build_and_upload_release.sh
+++ scripts/build_and_upload_release.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
 # TODO: consider to create / update the tag when running the release script
-# TODO: push changes to google code
-# TODO: find out how to save multiple push locations
 # claim commits for my other email address
 
 USAGE="Usage: $0 <bzr-tag-to-release> <googlecode-username> <googlecode-password>"
@@ -13,16 +11,22 @@ PASSWORD=${3:? $USAGE}
 
 cd "$(dirname "$0")/.."
 
-# update changelog
+echo "Updating changelog"
 bzr log --log-format=gnu-changelog > CHANGELOG
 bzr commit CHANGELOG -m "updated changelog before release"
 
-# build release zip
+echo "Tagging release"
+bzr tag "$RELEASE_TAG"
+
+echo "Pushing all changes to upstream (googlecode)"
+bzr push bm:upstream
+
+echo "Building release zip"
 mkdir -p build
 EXPORTED_FILE="build/jquery-editInPlace-v${RELEASE_TAG}.zip"
 bzr export --revision="$RELEASE_TAG" "$EXPORTED_FILE"
 
-# upload release zip
+echo "Uploading release zip"
 SUMMARY="jQuery In Place Editor v${RELEASE_TAG}"
 python scripts/googlecode_upload.py \
 	--summary="$SUMMARY" \
