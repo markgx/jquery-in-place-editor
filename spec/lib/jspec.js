bbecf1ba1a3304bf1404770d3b1dbed6481d8d48
mhaecker <mhaecker@145117a4-22cd-11de-b44d-8590aa00aed0>
2010-05-13 16:04:37 +0000

    updated to jspec 4.3.1

--- spec/lib/jspec.js
+++ spec/lib/jspec.js
@@ -4,7 +4,7 @@
 ;(function(){
 
   JSpec = {
-    version   : '4.2.1',
+    version   : '4.3.1',
     assert    : true,
     cache     : {},
     suites    : [],
@@ -1872,17 +1872,17 @@
     },
     
     have_prop : function(actual, property, value) {
-      return actual[property] === undefined ||
-               actual[property] instanceof Function ? false:
-                 value === undefined ? true:
-                   does(actual[property], 'eql', value)
+      var actualVal = actual[property], actualType = typeof actualVal
+      return (actualType == 'function' || actualType == 'undefined') ? false :
+        typeof value === 'undefined' ||
+        does(actual[property],'eql',value)
     },
     
     have_property : function(actual, property, value) {
-      return actual[property] === undefined ||
-               actual[property] instanceof Function ? false:
-                 value === undefined ? true:
-                   value === actual[property]
+      var actualVal = actual[property], actualType = typeof actualVal
+      return (actualType == 'function' || actualType == 'undefined') ? false :
+        typeof value === 'undefined' ||
+        value === actualVal
     }
   })
   
